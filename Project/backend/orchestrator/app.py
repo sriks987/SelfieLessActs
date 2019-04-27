dccd7512668c3dfa06aa725e31ccf4114f426830
Rhythm <rhythm.girdhar@creativesandcoders.co.in>
2019-04-28 02:14:44 +0530

    Theading with locks. Un-tested.

--- Project/backend/orchestrator/app.py
+++ Project/backend/orchestrator/app.py
@@ -16,12 +16,7 @@ from flask import Flask, request, Response, abort, render_template
 app = Flask(__name__)
 portLists = [8000]
 index = 0
-
-#global thread creation
-#balance_thread = threading.Thread(target=balance)
-scaling_thread = threading.Thread(target=scaling)
-healthcheck_thread = threading.Thread(target=monitorHealth)
-
+lock= threading.lock() 
 
 @app.route("/api/v1/<path:remaining>", methods=["GET", "POST", "PUT", "DELETE"])
 def balance(remaining):
@@ -51,41 +46,55 @@ def scaling():
                         diff = var[0]//20 - len(portLists)
                         if diff > 0:
                                 for i in range(0, diff):
+                                        lock.acquire()
                                         port = portLists[-1]+1
                                         portLists.append(port)
                                         os.execute('docker run -d -p '+ str(port) + ':80 acts')
+                                        lock.release()
                         else:
                                 for i in range(0, math.abs(diff)):
+                                        lock.acquire()
                                         port = portLists.pop()
                                         os.execute('docker rm $(docker ps -a | grep "'+str(port)+'>80/tcp" --force')
+                                        lock.release()
                 sleep(40)
 
 def monitorHealth():
     while True:
         for i in range(len(portList)):
+                lock.acquire()
                 portNumber = portList[i]
                 res = requests.get(url = 'http://localhost:' + str(portNumber) + '/api/v1/_health')
                 if res.status_code == 500:
                         os.execute('docker rm $(docker ps -a | grep "'+str(portNumber)+'>80/tcp" --force')
                         os.execute('docker run -d -p ' + str(portNumber) + ':80 acts')
+                lock.release()
         sleep(1)
 
 
 
 #threads = []
+#global thread creation
+#balance_thread = threading.Thread(target=balance)
+scaling_thread = threading.Thread(target=scaling)
+healthcheck_thread = threading.Thread(target=monitorHealth)
+lock= threading.lock() 
 
 if __name__ == '__main__':
 	
     #balance_thread.start()
     scaling_thread.start()
     healthcheck_thread.start()
-
-    #for t in threads:
-    #    t.join()
     
+    main_thread = threading.currentThread() 
+ 
+    app.debug == True
+    app.run(host='0.0.0.0', port=80, debug = True)
+   
     #balance_thread.join()
     scaling_thread.join()
     healthcheck_thread.join()
-
-    app.debug == True
-    app.run(host='0.0.0.0', port=80, debug = True)
+    #for t in threading.enumerate():
+    #    if t is not in main_thread:
+    #        t.join()
+    logging.debug("Done")
