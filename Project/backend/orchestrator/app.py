6169eaafac7f469e7767b6c232ffc164eb242a37
Priya Nayak <nayakpriya98@gmail.com>
2019-04-28 13:31:51 +0530

    Updated code.

--- Project/backend/orchestrator/app.py
+++ Project/backend/orchestrator/app.py
@@ -1,103 +1,114 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from flask import json
 import os
-import logging
-import json
 import requests
+import time
+import math
+from threading import Thread, Lock
+import subprocess, signal
+import logging
 from time import sleep
-import threading
-from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
-from flask import Flask, request, Response, abort, render_template
 
 app = Flask(__name__)
 portList = []
 index = 0
-startFlag = 0
-lock= threading.lock()
+lock= Lock()
+healthlock = Lock()
+incCount = Lock()
+requestCount = 0
+
 
 @app.route("/api/v1/<path:remaining>", methods=["GET", "POST", "PUT", "DELETE"])
 def balance(remaining):
-    global startFlag
-    if startFlag == 0:
-        scaling_thread.start()
-        startFlag = 1
-    global index
-    app.logger.warning(remaining)
-    if request.method == 'GET':
-        var = str(requests.get(url = 'http://127.0.0.1:' + str(portList[index]) + '/api/v1/' + remaining).json())
-    elif request.method == 'POST':
-        jsonPart = request.get_json()
-        var = str(requests.post(url = 'http://127.0.0.1:' + str(portList[index]) + '/api/v1/' + remaining, json = jsonPart).json())
-    elif request.method == 'PUT':
-        jsonPart = request.get_json()
-        var = str(requests.put(url = 'http://127.0.0.1:' + str(portList[index]) + '/api/v1/' + remaining, json = jsonPart).json())
-    elif request.method == 'DELETE':
-        jsonPart = request.get_json()
-        var = str(requests.delete(url = 'http://127.0.0.1:' + str(portList[index]) + '/api/v1/' + remaining, json = jsonPart).json())
-    app.logger.warning(portList[index])
-    index = (index + 1)%(len(portList))
-    return var
+	global index
+	global requestCount
+	app.logger.warning(remaining)
+	if request.method == 'GET':
+		var = requests.get(url="http://3.212.219.92:"+str(portList[index]) + "/api/v1/"+remaining)
+		r1 = var.json()
+		incCount.acquire()
+		requestCount = requestCount + 1
+		incCount.release()
+	elif request.method == 'POST':
+		jsonPart = request.get_json()
+		var = requests.post(url = 'http://3.212.219.92:' + str(portList[index]) + '/api/v1/' + remaining, json = jsonPart)
+		r1 = var.json()
+		incCount.acquire()
+		requestCount = requestCount + 1
+		incCount.release()
+	elif request.method == 'PUT':
+		jsonPart = request.get_json()
+		var = requests.put(url = 'http://3.212.219.92:' + str(portList[index]) + '/api/v1/' + remaining, json = jsonPart)
+		r1 = var.json()
+		incCount.acquire()
+		requestCount = requestCount + 1
+		incCount.release()
+	elif request.method == 'DELETE':
+		jsonPart = request.get_json()
+		var = requests.delete(url = 'http://3.212.219.92:' + str(portList[index]) + '/api/v1/' + remaining, json = jsonPart)
+		r1 = var.json()
+		incCount.acquire()
+		requestCount = requestCount + 1
+		incCount.release()
+
+	app.logger.warning(portList[index])
+	index = (index + 1)%(len(portList))
+	return json.jsonify(r1), var.status_code
 
 def scaling():
-    while True:
-        var = requests.get("http://localhost:8000/api/v1/_count").json()
-        app.logger.warning(var[0])
-        if var[0]//20 == len(portList) or len(portList) == 1:
-            continue
-        else:
-            diff = var[0]//20 - len(portList)
-            app.logger.warning(diff)
-            if diff > 0:
-                for i in range(0, diff):
-                    lock.acquire()
-                    port = portList[-1]+1
-                    portList.append(port)
-                    os.system('docker run -d -p '+ str(port) + ':80 acts')
-                    lock.release()
-            else:
-                for i in range(0, abs(diff)):
-                    port = portList.pop()
-                    os.system('docker rm $(docker ps -a | grep "'+str(port)+'->80/tcp" --force')
-        sleep(40)
+	global requestCount
+	while requestCount==0:
+		  pass
+	while True:
+		var = requestCount
+		if var//20 == len(portList) or len(portList) == 1:
+			continue
+		else:
+			diff = var//20 - len(portList)
+			app.logger.warning(diff)
+			if diff > 0:
+				for i in range(0, diff):
+					lock.acquire()
+					port = portList[-1]+1
+					portList.append(port)
+					os.system('docker run -d -p '+ str(port) + ':80 acts')
+					lock.release()
+			else:
+				for i in range(0, abs(diff)):
+					port = portList.pop()
+					os.system('docker rm $(docker ps -a | grep "'+str(port)+'->80/tcp" --force')
+		sleep(120)
+	requestCount = 0
+
+scaling_thread = Thread(target=scaling)
 
 def monitorHealth():
-    while True:
-        for i in range(len(portList)):
-            lock.acquire()
-            portNumber = portList[i]
-            res = requests.get(url = 'http://localhost:' + str(portNumber) + '/api/v1/_health')
-            if res.status_code == 500:
-                os.system('docker rm $(docker ps -a | grep "'+str(portNumber)+'>80/tcp" --force')
-                os.system('docker run -d -p ' + str(portNumber) + ':80 acts')
-            lock.release()
-        sleep(1)
-
+	while True:
+		for i in range(len(portList)):
+			healthlock.acquire()
+			portNumber = portList[i]
+			res = requests.get(url = 'http://localhost:' + str(portNumber) + '/api/v1/_health')
+			if res.status_code == 500:
+				os.system('docker rm $(docker ps -a | grep "'+str(portNumber)+'>80/tcp" --force')
+				os.system('docker run -d -p ' + str(portNumber) + ':80 acts')
+			healthlock.release()
+		sleep(1)
+healthcheck_thread = Thread(target=monitorHealth)
+healthcheck_thread.start()
 
 def callBalance():
-    #app.debug == True
-    app.run(host='0.0.0.0', port=80)
+	#app.debug == True
+	app.run(host='0.0.0.0', port=80)
 
-#threads = []
-scaling_thread = threading.Thread(target=scaling)
-healthcheck_thread = threading.Thread(target=monitorHealth)
-app = threading.Thread(target=callBalance)
-
-
-app.start()
+appnew = Thread(target=callBalance)
 
 def initializeContainer():
-    os.system("docker run -d -p 8000:80 acts")
-    portList.append(8000)
+	os.system("docker run -d -p 8000:80 acts")
+	portList.append(8000)
 
 initializeContainer()
 
 if __name__ == '__main__':
-    
-    scaling_thread.start()
-    healthcheck_thread.start()
-    # don't join because it is an infinite loop
-    # need to keep running indefinitely.
-    
-    #main_thread = threading.currentThread() 
-    #for t in threading.enumerate():
-    #    if t is not in main_thread:
-    #        t.join()
-    logging.debug("Done")
+	appnew.start()
+	scaling_thread.start()
+	logging.debug("Done")
